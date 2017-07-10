dfbca5d6025b38b3db95835f79bad5a124b88481
unknown <pau.lopez.78@gmail.com>
2017-07-10 14:21:21 +0200

    first commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using LoggingLib;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger.Log("Starting app");
+            
+            Logger.LogInfo("Some info data");
+
+            Logger.LogDebug("Some debug data");
+
+            Console.WriteLine(Logger.Dump());
+
+            Logger.SaveLog("consoleapp-log.xml");
+        }
+    }
+}
